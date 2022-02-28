b70b6dc4ba0d7c2a235c895594a8f9eccc0854d7
niikwartey <niikwartey1@gmail.com>
2022-02-27 19:55:18 -0500

    Updates

---
+++ precache-manifest.323304a9fda0c010d362d575e95a65d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc8261d6de4b7362e61",
+    "url": "/robopals/static/css/main.3cc0bb55.chunk.css"
+  },
+  {
+    "revision": "ccc8261d6de4b7362e61",
+    "url": "/robopals/static/js/main.ccc8261d.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/robopals/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "c10cd2c5be979cb04abb",
+    "url": "/robopals/static/css/2.22ab8d0f.chunk.css"
+  },
+  {
+    "revision": "c10cd2c5be979cb04abb",
+    "url": "/robopals/static/js/2.c10cd2c5.chunk.js"
+  },
+  {
+    "revision": "c18bb7814d506f1a953a",
+    "url": "/robopals/static/js/runtime~main.c18bb781.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robopals/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a4bcc85d03870653b1b2eef1ad00f7c9",
+    "url": "/robopals/index.html"
+  }
+];
